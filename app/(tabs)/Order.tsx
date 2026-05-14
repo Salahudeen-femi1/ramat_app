7cb9ac320e61b36fc4faa06bc0c987466c0fbf44
Salahudeen-femi1 <102353790+Salahudeen-femi1@users.noreply.github.com>
2026-05-14 18:24:34 +0100

    ramat mobile app

--- app/(tabs)/Order.tsx
+++ app/(tabs)/Order.tsx
@@ -4,50 +4,51 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import CompleteOrder from "../pages/CompleteOrder";
 import OngoingOrder from "../pages/OngoingOrder";
 
-export default function TaskTabs() {
-  const [status, setStatus] = useState<'ongoing' | 'completed'>('ongoing');
+type StatusType = "ongoing" | "completed";
 
-  const tabs = [
+export default function TaskTabs() {
+
+  const [status, setStatus] = useState<StatusType>("ongoing");
+
+  const tabs: { label: string; value: StatusType }[] = [
     { label: "Ongoing", value: "ongoing" },
     { label: "Completed", value: "completed" },
   ];
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-    <View className="mt-8 mr-8">
+      <View className="mt-8 mr-8">
 
-      {/* Tabs */}
-      <View className="bg-gray-300 rounded-md m-5 px-2 py-5 flex-row items-center justify-center">
-        {tabs.map((tab, index) => {
-          const active = status === tab.value;
+        {/* Tabs */}
+        <View className="bg-gray-300 rounded-md m-5 px-2 py-5 flex-row items-center justify-center">
+          {tabs.map((tab, index) => {
+            const active = status === tab.value;
 
-          return (
-            <Pressable
-              key={index}
-              onPress={() => setStatus(tab.value)}
-              className={`px-6 w-48 py-5 flex-1 text-center rounded-md mr-5 ${
-                active ? "bg-primary" : "bg-transparent"
-              }`}
-            >
-              <Text
-                className={`font-medium text-center ${
-                  active ? "text-white" : "text-black"
-                }`}
+            return (
+              <Pressable
+                key={index}
+                onPress={() => setStatus(tab.value)}
+                className={`px-6 w-48 py-5 flex-1 text-center rounded-md mr-5 ${active ? "bg-primary" : "bg-transparent"
+                  }`}
               >
-                {tab.label}
-              </Text>
-            </Pressable>
-          );
-        })}
-      </View>
+                <Text
+                  className={`font-medium text-center ${active ? "text-white" : "text-black"
+                    }`}
+                >
+                  {tab.label}
+                </Text>
+              </Pressable>
+            );
+          })}
+        </View>
 
-      {/* Content */}
-      <View className="mt-6">
-        {status === "ongoing" && <OngoingOrder />}
-        {status === "completed" && <CompleteOrder />}
-      </View>
+        {/* Content */}
+        <View className="mt-6">
+          {status === "ongoing" && <OngoingOrder />}
+          {status === "completed" && <CompleteOrder />}
+        </View>
 
-    </View>
+      </View>
 
     </SafeAreaView>
   );
