c457c07902757bdc5af089df5d71926aedd00a34
Salahudeen-femi1 <102353790+Salahudeen-femi1@users.noreply.github.com>
2026-08-05 15:39:29 +0100

    update

--- app/(auth)/register/index.tsx
+++ app/(auth)/register/index.tsx
@@ -36,7 +36,7 @@ export default function Register() {
         data.message || "Registration successful. Please verify your email."
       );
       console.log("register response", data);
-      // router.push({ pathname: "/(auth)/verify_email", params: { email: variables.email } });
+      router.push({ pathname: "/(auth)/verify_email", params: { email: variables.email } });
       router.push('/(tabs)')
     },
     onError: (err: any) => {
